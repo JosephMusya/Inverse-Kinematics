61c019b6654d9f7f891b96894f05cc6335ddc329
Musya Joseph <57936020+JosephMusya@users.noreply.github.com>
2022-03-10 11:48:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
+'''
+Not that this code has been developed for testing purpose only.
+The structure of the robot is defined in the trajectory library
+in kinematics section
+'''
+
 from trajectory import goTo
-import random as rd
-while True:
-    #Generate random  positions
-    x = rd.randint(4,7) 
-    y = rd.randint(-1,1)
-    z = rd.randint(8,9)
-    goTo(HOME_POS=[11,0,8],TARGET=[x,y,z])
-    print("_____________________________")
+
+#Defining the start and the end position of the robot arm
+HOME_POS = [11,0,2]
+TARGET = [3,7,0]
+t = 5
+H0_6 = [ #Orientation of the end effector
+    [0,1,0,0],
+    [0,0,1,0],
+    [1,0,0,0],
+    [0,0,0,1]
+]
+#Calling the function to execute the motion
+goTo(HOME_POS,
+     TARGET,
+     t,
+     H0_6)
